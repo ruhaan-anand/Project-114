60f960189585f8b939bd76622d1c7a055c88a285
ruhaan-anand <73781091+ruhaan-anand@users.noreply.github.com>
2022-03-20 16:35:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.center()    
+}
+
+function draw(){}
+
+function take_snapshot(){
+    save("my-image.png")
+}
